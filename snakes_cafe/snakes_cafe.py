49197dd3af3ef1d7b04d1dfb20abbf852b7ed399
Logan Jones <loganemilyjones@mac.com>
2020-12-07 12:15:34 -0800

    Better case handling

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,15 +1,15 @@
 # Variables
-quit_command = 'Quit'
+quit_command = 'quit'
 
 menu = [
   #appetizers
-  "Wings", "Cookies", "Spring Rolls",
+  "wings", "cookies", "spring rolls",
   #entrees
-  "Salmon", "Steak", "Meat Tornado", "A Literal Garden",
+  "salmon", "steak", "meat tornado", "a literal garden",
   #desserts
-  "Ice Cream", "Cake", "Pie",
+  "ice cream", "cake", "pie",
   #drinks
-  "Coffee", "Tea", "Unicorn Tears"
+  "coffee", "tea", "unicorn tears"
 ]
 
 order = {}
@@ -58,19 +58,19 @@ def print_complete_order():
   print("""***********************************
 Current Order:
   """)
-  for key in order:
-    print(order[key], key)
+  for (item, qty) in order.items():
+    print(qty, item.title())
   print("***********************************\n")
 
 def print_order(qty, item):
   if qty > 1:
-    print('\n** {} orders of {} has been added to your meal **\n'.format(order[item], item))
+    print('\n** {} orders of {} has been added to your meal **\n'.format(order[item], item.title()))
   else:
-    print('\n** 1 order of {} has been added to your meal **\n'.format(item))
+    print('\n** 1 order of {} has been added to your meal **\n'.format(item.title()))
 
 def take_order():
   # Order prompt
-  item = input(order_prompt).title()
+  item = input(order_prompt).strip().lower()
 
   if item == quit_command:
     quit()
